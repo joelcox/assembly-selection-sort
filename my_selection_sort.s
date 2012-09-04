5ef20eda0836d91036b3172e62929ffd2d86e212
Joël Cox <joel@joelcox.nl>
2012-09-04 17:41:24 +0200

    Modified assembly for gcc 4.2.1

--- my_selection_sort.s
+++ my_selection_sort.s
@@ -1,9 +1,9 @@
 	.intel_syntax noprefix
 	.code32
 	.text
-	.globl my_selection_sort	# bij foutmelding evtl. veranderen in:
+	.globl _my_selection_sort	# bij foutmelding evtl. veranderen in:
 					# .globl _my_selection_sort
-my_selection_sort:			# bij foutmelding evtl. veranderen in:
+_my_selection_sort:			# bij foutmelding evtl. veranderen in:
 					# _my_selection_sort:
 	push	ebp
 	mov	ebp, esp
@@ -16,12 +16,12 @@ my_selection_sort:			# bij foutmelding evtl. veranderen in:
 	# Het is niet nodig iets aan de instructies hierboven te
 	# veranderen. Voeg je programma hieronder in.
 
-	...
-	...
-	...
+	#...
+	#...
+	#...
 
 	# zet eax op het resultaat van de functie
-	mov	eax, ...
+	#mov	eax, ...
 
 	# Het is niet nodig iets aan de instructies hieronder te
 	# veranderen.
